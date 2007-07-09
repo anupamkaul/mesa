ae108bfaae54f0562ea80336753fa1c7a1040b20
Brian <brian.paul@tungstengraphics.com>
2007-07-09 15:45:57 -0600

    fix stage, stage->next mix-up

--- src/mesa/clip/clip_pipe_offset.c
+++ src/mesa/clip/clip_pipe_offset.c
@@ -123,7 +123,7 @@ static void offset_tri( struct clip_pipe_stage *stage,
    tmp.v[1] = dup_vert(stage, header->v[1], 1);
    tmp.v[2] = dup_vert(stage, header->v[2], 2);
 
-   do_offset_tri( stage->next, &tmp );
+   do_offset_tri( stage, &tmp );
 }
 
 
