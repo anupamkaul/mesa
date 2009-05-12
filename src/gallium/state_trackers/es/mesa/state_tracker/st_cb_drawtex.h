3a1193ae2113a9b6be150760bc493e77b1ca5018
Brian Paul <brianp@vmware.com>
2009-05-12 12:30:20 -0600

    st: ES state tracker code for glDrawTex*OES()

---
+++ src/gallium/state_trackers/es/mesa/state_tracker/st_cb_drawtex.h
@@ -0,0 +1,20 @@
+/**************************************************************************
+ * 
+ * Copyright 2008 Tungsten Graphics, Inc., Cedar Park, Texas.
+ * All Rights Reserved.
+ *
+ **************************************************************************/
+
+
+#ifndef ST_CB_DRAWTEX_H
+#define ST_CB_DRAWTEX_H
+
+extern void
+_mesa_DrawTexf(GLfloat x, GLfloat y, GLfloat z, GLfloat width, GLfloat height);
+
+
+extern void
+st_destroy_drawtex(struct st_context *st);
+
+
+#endif
