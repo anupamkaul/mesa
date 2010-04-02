5c670481248c4d46f87f13bf3af5655925e7002d
Brian Paul <brianp@vmware.com>
2010-04-02 16:36:11 -0600

    llvmpipe: add a special-case optimization to lp_surface_copy()
    Be more efficient when copying tiled image to linear image.
    Before, the fallback path was always converting the whole source image
    to linear.  Now we can convert just a sub region.

--- src/gallium/drivers/llvmpipe/lp_surface.c
+++ src/gallium/drivers/llvmpipe/lp_surface.c
@@ -29,16 +29,37 @@
 #include "lp_context.h"
 #include "lp_flush.h"
 #include "lp_surface.h"
+#include "lp_texture.h"
+#include "lp_tile_image.h"
+#include "lp_tile_size.h"
+
+
+/**
+ * Adjust x, y, width, height to lie on tile bounds.
+ */
+static void
+adjust_to_tile_bounds(unsigned x, unsigned y, unsigned width, unsigned height,
+                      unsigned *x_tile, unsigned *y_tile,
+                      unsigned *w_tile, unsigned *h_tile)
+{
+   *x_tile = x & ~(TILE_SIZE - 1);
+   *y_tile = y & ~(TILE_SIZE - 1);
+   *w_tile = ((x + width + TILE_SIZE - 1) & ~(TILE_SIZE - 1)) - *x_tile;
+   *h_tile = ((y + height + TILE_SIZE - 1) & ~(TILE_SIZE - 1)) - *y_tile;
+}
+
 
 
 static void
 lp_surface_copy(struct pipe_context *pipe,
-                struct pipe_surface *dest, unsigned destx, unsigned desty,
+                struct pipe_surface *dst, unsigned dstx, unsigned dsty,
                 struct pipe_surface *src, unsigned srcx, unsigned srcy,
                 unsigned width, unsigned height)
 {
+   enum lp_texture_layout src_layout, dst_layout;
+
    llvmpipe_flush_texture(pipe,
-                          dest->texture, dest->face, dest->level,
+                          dst->texture, dst->face, dst->level,
                           0, /* flush_flags */
                           FALSE, /* read_only */
                           FALSE, /* cpu_access */
@@ -51,8 +72,62 @@ lp_surface_copy(struct pipe_context *pipe,
                           FALSE, /* cpu_access */
                           FALSE); /* do_not_flush */
 
+   /* Look for special case in which we're copying from a tiled image
+    * to a linear image.
+    */
+   {
+      struct llvmpipe_texture *src_tex = llvmpipe_texture(src->texture);
+      struct llvmpipe_texture *dst_tex = llvmpipe_texture(dst->texture);
+      enum pipe_format format = src_tex->base.format;
+
+      src_layout = llvmpipe_get_texture_image_layout(src_tex,
+                                                     src->face, src->level);
+
+      dst_layout = llvmpipe_get_texture_image_layout(dst_tex,
+                                                     dst->face, dst->level);
+
+      if (src_layout == LP_TEXTURE_TILED &&
+          dst_layout == LP_TEXTURE_LINEAR) {
+         unsigned tx, ty, tw, th;
+         ubyte *src_tiled_ptr = src_tex->tiled[src->face][src->level].data;
+         ubyte *src_linear_ptr = src_tex->linear[src->face][src->level].data;
+         ubyte *dst_linear_ptr = dst_tex->linear[dst->face][dst->level].data;
+
+         if (src_linear_ptr && src_tiled_ptr && dst_linear_ptr) {
+            adjust_to_tile_bounds(srcx, srcy, width, height,
+                                  &tx, &ty, &tw, &th);
+
+            /* convert tiled src data to linear */
+            lp_tiled_to_linear(src_tiled_ptr,
+                               src_linear_ptr,
+                               tx, ty, tw, th,
+                               format,
+                               dst_tex->stride[dst->level]);
+
+            util_copy_rect(dst_linear_ptr, format,
+                           dst_tex->stride[dst->level],
+                           dstx, dsty,
+                           width, height,
+                           src_linear_ptr, src_tex->stride[src->level],
+                           srcx, srcy);
+
+            if (width == src->width && height == src->height) {
+               assert(srcx == 0);
+               assert(srcy == 0);
+               /* We converted the whole src image to linear.
+                * Update the timestamp to indicate equality.
+                */
+               src_tex->linear[src->face][src->level].timestamp =
+                  src_tex->linear[src->face][src->level].timestamp;
+            }
+
+            return;
+         }
+      }
+   }
+
    util_surface_copy(pipe, FALSE,
-                     dest, destx, desty,
+                     dst, dstx, dsty,
                      src, srcx, srcy,
                      width, height);
 }
