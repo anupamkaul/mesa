8cf8328cf31e562c5ab31f9f3e25428fbec0bbdd
Brian Paul <brianp@vmware.com>
2010-10-06 10:25:06 -0600

    gallivm: better illegal function #defines

--- src/gallium/auxiliary/gallivm/lp_bld.h
+++ src/gallium/auxiliary/gallivm/lp_bld.h
@@ -57,28 +57,33 @@
 
 extern LLVMContextRef LC;
 
-#define LLVMInt1Type FOO
-#define LLVMInt8Type FOO
-#define LLVMInt16Type FOO
-#define LLVMInt32Type FOO
-#define LLVMInt64Type FOO
-#define LLVMIntType FOO
-#define LLVMFloatType FOO
-#define LLVMDoubleType FOO
-#define LLVMX86FP80Type FOO
-#define LLVMFP128Type FOO
-#define LLVMPPCFP128Type FOO
-#define LLVMStructType FOO
-#define LLVMVoidType FOO
-#define LLVMLabelType FOO
-#define LLVMOpaqueType FOO
-#define LLVMUnionType FOO
-#define LLVMMDString FOO
-#define LLVMMDNode FOO
-#define LLVMConstString FOO
-#define LLVMConstStruct FOO
-#define LLVMAppendBasicBlock FOO
-#define LLVMInsertBasicBlock FOO
-#define LLVMCreateBuilder FOO
+/**
+ * Redefine these LLVM entrypoints as invalid macros to make sure we
+ * don't accidentally use them.  We need to use the functions which
+ * take and explicit LLVMContextRef parameter.
+ */
+#define LLVMInt1Type ILLEGAL_LLVM_FUNCTION
+#define LLVMInt8Type ILLEGAL_LLVM_FUNCTION
+#define LLVMInt16Type ILLEGAL_LLVM_FUNCTION
+#define LLVMInt32Type ILLEGAL_LLVM_FUNCTION
+#define LLVMInt64Type ILLEGAL_LLVM_FUNCTION
+#define LLVMIntType ILLEGAL_LLVM_FUNCTION
+#define LLVMFloatType ILLEGAL_LLVM_FUNCTION
+#define LLVMDoubleType ILLEGAL_LLVM_FUNCTION
+#define LLVMX86FP80Type ILLEGAL_LLVM_FUNCTION
+#define LLVMFP128Type ILLEGAL_LLVM_FUNCTION
+#define LLVMPPCFP128Type ILLEGAL_LLVM_FUNCTION
+#define LLVMStructType ILLEGAL_LLVM_FUNCTION
+#define LLVMVoidType ILLEGAL_LLVM_FUNCTION
+#define LLVMLabelType ILLEGAL_LLVM_FUNCTION
+#define LLVMOpaqueType ILLEGAL_LLVM_FUNCTION
+#define LLVMUnionType ILLEGAL_LLVM_FUNCTION
+#define LLVMMDString ILLEGAL_LLVM_FUNCTION
+#define LLVMMDNode ILLEGAL_LLVM_FUNCTION
+#define LLVMConstString ILLEGAL_LLVM_FUNCTION
+#define LLVMConstStruct ILLEGAL_LLVM_FUNCTION
+#define LLVMAppendBasicBlock ILLEGAL_LLVM_FUNCTION
+#define LLVMInsertBasicBlock ILLEGAL_LLVM_FUNCTION
+#define LLVMCreateBuilder ILLEGAL_LLVM_FUNCTION
 
 #endif /* LP_BLD_H */
